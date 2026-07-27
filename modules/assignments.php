6baa1f68b68882bad7b056ffa24388acf1e62533
zulaika256 <zulaikafatin256@gmail.com>
2026-07-28 02:10:02 +0800

    Delete modules directory

--- modules/assignments.php
+++
@@ -1,4 +0,0 @@
-<?php
-// DELETED: Assignments module has been completely removed from the YAWATIM system.
-header("Location: ../index.php");
-exit;
--- modules/booths.php
+++
@@ -1,4 +0,0 @@
-<?php
-// DELETED: Booths management has been completely replaced by the Wakalah Corporate module.
-header("Location: ../index.php");
-exit;
